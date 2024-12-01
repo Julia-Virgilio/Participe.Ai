59a65e3059db87cb28399afcdd54d07ca019ff99
kevensantana <kevensantana40@gmail.com>
2024-12-01 15:28:39 -0300

    Cadastro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from "./shared/components/login/login.component";
+import { CadastroComponent } from "./shared/components/cadastro/cadastro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [CadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
