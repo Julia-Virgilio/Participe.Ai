3e298af0b6ff77ef5ad50c8606b2f648be36b362
Bowie <reislizandrasantos@gmail.com>
2024-12-07 15:22:12 -0300

    atualizações front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,20 +10,42 @@ import { EnergiaComponent } from './pages/sub/energia/energia.component';
 import { ReciclagemComponent } from './pages/sub/reciclagem/reciclagem.component';
 import { MapaComponent } from './pages/mapa/mapa.component';
 import { FaqComponent } from './pages/faq/faq.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { CadastroComponent } from './shared/components/cadastro/cadastro.component';
 
 export const routes: Routes = [
 
-  { path: '',  component: DashboardComponent,},
-  {path: 'home', component: HomeComponent,},
-  { path: 'perfil',  component: PerfilComponent,},
-  { path: 'aprendizado',  component: AprendizadoComponent,},
-  { path: 'map', component: MapaComponent },
-  { path: 'faq', component: FaqComponent },
+  {path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+  {path: '', 
+    component: AppComponent,
+    children:[
+      {path: 'home', component: HomeComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'cadastro', component: CadastroComponent}
+    ]
+  },
+  {path: '', 
+    component: MainLayoutComponent,
+    children:[
+      { path: 'dashboard',  component: DashboardComponent,},
+      { path: 'perfil',  component: PerfilComponent,},
+      { path: 'aprendizado',  component: AprendizadoComponent,},
+      { path: 'map', component: MapaComponent },
+      { path: 'faq', component: FaqComponent },
+      { path: 'agua', component: AguaComponent },
+      { path: 'energia', component: EnergiaComponent },
+      { path: 'reciclagem', component: ReciclagemComponent },
+    ]
+  },
 
 
-  { path: 'agua', component: AguaComponent },
-  { path: 'energia', component: EnergiaComponent },
-  { path: 'reciclagem', component: ReciclagemComponent },
+
   
   // { path: 'sobre/gamificacao', component: gamificacao },
   // { path: 'feedback', component: FeedbackComponent },
